d2691e1aebd024afd51b56944f77e5bf3ed92467
m-Cac <matt.cacioppo8@gmail.com>
2020-05-19 12:52:24 -0500

    Maze refactoring with new Maze Class

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -1,15 +1,8 @@
 import java.util.LinkedList;
 
 public class MazeSolver {
+	static Maze m = new Maze();
 	
-	static int[][] maze = {
-			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
-			{0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
-			{0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0},
-			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
-			{1, 2, 1, 1, 1, 1, 0, 0, 0, 1, 0},
-			{0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0}
-	};
 	//0 = wall 
 	//1 = path
 	//2 = destination
@@ -18,74 +11,88 @@ public class MazeSolver {
 
 	public static void main(String[] args) {
 		
-		if(solveMaze(new Position(4, 9))) {
+		int[][] maze = {
+			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+			{0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
+			{0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0},
+			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+			{1, 2, 1, 1, 1, 1, 0, 0, 0, 1, 0},
+			{0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0}
+		};
+		m.maze = maze;
+		m.start = new Position(4, 8);
+		m.path = new LinkedList<Position>();
+		 
+		if(solveMaze(m.start)) {
 			System.out.println("You won!!!");
 			return;
 		}
 		System.out.println("No path");
 		return;
 	}
+	
+	
 	private static boolean solveMaze(Position p) {
-		path.push(p);
+		m.path.push(p);
         
 		while(true) {
-			int y = path.peek().y;
-			int x = path.peek().x;
-			maze[y][x] = 0;
+			int y =m.path.peek().y;
+			int x = m.path.peek().x;
+			m.maze[y][x] = 0;
 			
 			//down
 			if(isValid(y+1, x)) {
-				if(maze[y+1][x] == 2) {
+				if(m.maze[y+1][x] == 2) {
 					return true;
 				}
-				else if(maze[y+1][x] == 1) {
-					path.push(new Position(y+1, x));
+				else if(m.maze[y+1][x] == 1) {
+					m.path.push(new Position(y+1, x));
 					System.out.println("Moved down");
 					continue;
 				 }
 			}
 			//left
 			if(isValid(y, x-1)) {
-				if(maze[y][x-1] == 2) {
+				if(m.maze[y][x-1] == 2) {
 					return true;
 				}
-				else if (maze[y][x-1] == 1) {
-					path.push(new Position(y, x-1));
+				else if (m.maze[y][x-1] == 1) {
+					m.path.push(new Position(y, x-1));
 					System.out.println("Moved left");
 					continue;
 				}
 			}
 			//up
 			if(isValid(y-1, x)) {
-				if(maze[y-1][x] == 2) {
+				if(m.maze[y-1][x] == 2) {
 					return true;
 				}
-				else if (maze[y-1][x] == 1) {
-					path.push(new Position(y-1, x));
+				else if (m.maze[y-1][x] == 1) {
+					m.path.push(new Position(y-1, x));
 					continue;
 				}
 				
 			}
 			//right
 			if(isValid(y, x+1)) {
-				if(maze[y][x+1] == 2) {
+				if(m.maze[y][x+1] == 2) {
 					return true;
 				}
-				else if (maze[y][x+1] == 1) {
-					path.push(new Position(y, x+1));
+				else if (m.maze[y][x+1] == 1) {
+					m.path.push(new Position(y, x+1));
 					System.out.println("Moved right");
 					continue;
 				}
 			}
-			path.pop();
+			m.path.pop();
 			System.out.println("Moved back");
-			if(path.size() <= 0) {
+			if(m.path.size() <= 0) {
 				return false;
 			}
 		}
 	}
 	public static boolean isValid(int y, int x ) {
-		if (y < 0 || y >= maze.length || x < 0 || x >= maze[y].length ) {
+		if (y < 0 || y >= m.maze.length || x < 0 || x >= m.maze[y].length ) {
 			return false;
 		}	
 		return true;
